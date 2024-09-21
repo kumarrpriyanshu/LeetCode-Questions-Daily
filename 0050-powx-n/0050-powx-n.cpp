ab3bd6e05eec0147c07b5c77ada383323f5c54d1
kumarrpriyanshu <112817096+kumarrpriyanshu@users.noreply.github.com>
2024-09-21 11:56:00 +0530

    Time: 4 ms (21.88%), Space: 8 MB (97.34%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long binary=n;
+        if(n<0){
+            x=1/x;
+            binary=-binary;
+        }
+        
+        double ans=1;
+        while(binary>0){
+            if(binary%2==1){
+                ans*=x;
+            }
+            x*=x;
+            binary/=2;
+        }
+        return ans;
+
+
+        
+    }
+};
